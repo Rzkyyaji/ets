b4202863f3d0f0cd1a1c78c3431a81f6e96400cb
Ardeansyah <dardiansyah215@gmail.com>
2025-04-16 17:29:26 +0700

    edit buku,  admin

--- perpus-sdgs/DaftarBuku.php
+++ perpus-sdgs/DaftarBuku.php
@@ -2,6 +2,9 @@
 session_start();
 include 'koneksi.php';
 
+// Memeriksa apakah user sudah login dan apakah role-nya adalah admin
+$isAdmin = isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
+
 $query = "SELECT * FROM books";
 $result = mysqli_query($koneksi, $query);
 
@@ -15,8 +18,14 @@ if (mysqli_num_rows($result) > 0) {
                 <th>Tahun</th>
                 <th>ISBN</th>
                 <th>Kategori</th>
-                <th>Stok</th>
-            </tr>";
+                <th>Stok</th>";
+
+    // Tambahkan kolom "Edit" jika user adalah admin
+    if ($isAdmin) {
+        echo "<th>Actions</th>";
+    }
+
+    echo "</tr>";
 
     while ($row = mysqli_fetch_assoc($result)) {
         $coverPath = "Cover/" . htmlspecialchars($row['cover_image']);
@@ -32,8 +41,14 @@ if (mysqli_num_rows($result) > 0) {
                 <td>" . htmlspecialchars($row['year']) . "</td>
                 <td>" . htmlspecialchars($row['isbn']) . "</td>
                 <td>" . htmlspecialchars($row['category']) . "</td>
-                <td>" . htmlspecialchars($row['stock']) . "</td>
-              </tr>";
+                <td>" . htmlspecialchars($row['stock']) . "</td>";
+
+        // Menampilkan tombol edit hanya jika user adalah admin
+        if ($isAdmin) {
+            echo "<td><a href='editBuku.php?id=" . $row['id'] . "'>Edit</a></td>";
+        }
+
+        echo "</tr>";
     }
 
     echo "</table>";
